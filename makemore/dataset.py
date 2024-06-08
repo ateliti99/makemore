a2f3c1f2f1d3bdf4224ba939c9f081631171fc7e
ateliti99 <aurelio.teliti@gmail.com>
2024-06-08 19:02:23 +0200

    Remove unused template files and update dataset.py

--- makemore/dataset.py
+++ makemore/dataset.py
@@ -1,10 +1,13 @@
 from pathlib import Path
 
 import typer
+import requests
 from loguru import logger
 from tqdm import tqdm
 
 from makemore.config import PROCESSED_DATA_DIR, RAW_DATA_DIR
+
+DATASET_URL = "https://raw.githubusercontent.com/karpathy/makemore/master/names.txt"
 
 app = typer.Typer()
 
@@ -12,16 +15,16 @@ app = typer.Typer()
 @app.command()
 def main(
     # ---- REPLACE DEFAULT PATHS AS APPROPRIATE ----
-    input_path: Path = RAW_DATA_DIR / "dataset.csv",
-    output_path: Path = PROCESSED_DATA_DIR / "dataset.csv",
+    input_path: Path = RAW_DATA_DIR / "names.txt",
     # ----------------------------------------------
 ):
     # ---- REPLACE THIS WITH YOUR OWN CODE ----
-    logger.info("Processing dataset...")
-    for i in tqdm(range(10), total=10):
-        if i == 5:
-            logger.info("Something happened for iteration 5.")
-    logger.success("Processing dataset complete.")
+    logger.info("Downloading dataset...")
+    for i in tqdm(range(1), total=1):
+        response = requests.get(DATASET_URL)
+        with open(input_path, "wb") as f:
+            f.write(response.content)
+    logger.info("namex.txt correctly download!")
     # -----------------------------------------
 
 
